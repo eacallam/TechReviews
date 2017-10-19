25428fee8cfb8a71497dd465518e445cddac0858
eacallam <eacallam@yahoo.com>
2017-10-18 20:33:10 -0400

    First Commit

---
+++ TechReviews/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TechReviews.Models
+{
+    public class Category
+    {
+        //Created primary key and added System.ComponentModel.DataAnnotations
+        //Created ICollection to have access to Review properties
+
+        [Key] 
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Review> Reviews { get; set; }
+    }
+}
---
+++ TechReviews/Models/Review.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TechReviews.Models
+{
+    public class Review
+    {
+
+        [Key]
+        public int ID { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime PublishDate { get; set; }
+
+        //Created ForeignKey to access the Category model
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+    }
+}
