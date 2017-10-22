b361efd9e4500db3fdfb5824eb6ca61d2c49e446
eacallam <eacallam@yahoo.com>
2017-10-21 21:31:09 -0400

    Additional Work Completed

--- TechReviews/Models/Review.cs
+++ TechReviews/Models/Review.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -15,12 +16,14 @@ namespace TechReviews.Models
         public string Author { get; set; }
         public string Title { get; set; }
         [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        [DisplayName("Date Visited")]
         public DateTime DateVisited { get; set; }
         [Required(ErrorMessage = "A rating is required")]
         [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5")]
         public int Rating { get; set;  }
         private DateTime? dateCreated;
         [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        [DisplayName("Date of Review")]
         public DateTime DateCreated
         {
             get { return dateCreated ?? DateTime.Now; }
@@ -31,6 +34,7 @@ namespace TechReviews.Models
         //Created ForeignKey to access the Category model
         [ForeignKey("Category")]
         public int CategoryID { get; set; }
+        [DisplayName("Category")]
         public virtual Category Category { get; set; }
 
     }
